377596f05a71b8eafd48508bc501cb641617fa00
Mike Davis <mikedavis@laurenshope.com>
2023-04-12 12:02:24 -0500

    Initial push

---
+++ Models/Account/ConfirmModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebAdvert.Web.Models.Account
+{
+  public class ConfirmModel
+  {
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Code is required")]
+    public string Code { get; set; }
+  }
+}
